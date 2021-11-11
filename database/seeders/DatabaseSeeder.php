0736d68bf0534551822991def57b713ab8be81c2
George Vlisidis <gblisidis@gmail.com>
2021-11-11 09:53:01 +0000

    Installed Breeze and Inertia

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+         \App\Models\User::factory()->create([
+             'name' => 'George Vlisidis',
+             'email' => 'gv@mail.com'
+         ]);
     }
 }
